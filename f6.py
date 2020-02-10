2be76a8fa6898690b49de8af169cac413018e20a
AlexAndronati <55084387+AlexAndronati@users.noreply.github.com>
2020-02-10 21:25:23 +0200

    Update f6.py

--- f6.py
+++ f6.py
@@ -1,8 +1,11 @@
-
-
-l = [1, 2, 3, 4, 5]
+#l = [1, 2, 3, 4, 5]
+l = list(range(1000000))
 
 while True:
-    left = input()
-    right = input()
-    
+    left = int(input())
+    right = int(input())
+    counter = 0
+    for i in l[left:right]:
+        counter += i
+    print(counter)
+    break
