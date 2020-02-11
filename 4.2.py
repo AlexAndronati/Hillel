c559c989503a49ce7f475fab7220259966537b3c
AlexAndronati <55084387+AlexAndronati@users.noreply.github.com>
2020-02-11 14:55:46 +0200

    Update 4.2.py

--- 4.2.py
+++ 4.2.py
@@ -1,18 +1,23 @@
 
+
 def find_sum(target_list):
     suitable_list = []
-    for i in target_list:
+    for i in range(len(target_list)):
         a = str(format(i, 'b'))  # представление в двоичном виде
 
-        b = sum(int(j) for j in a if j != '-') # поиск суммы
-        # print(i, " ", a, ": ", b)
+        b = sum(int(j) for j in a if j != '-')  # поиск суммы
+        #  print(i, " ", a, ": ", b)
         if not b % 2:
-            suitable_list.append(i)
-    # print(suitable_list)
+            suitable_list.append(target_list[i])
+    #  print(suitable_list)
+
     return sum(suitable_list)
 
 
-l = [1, 2, 4, 6, 8, 9, 15, -4, -3, 14]
-print(l)
+# l = [1, 2, 4, 6, 8, 9, 15, -4, -3, 14]
+l = [1, 2, 3, 4, 5]
+print(f"l = {l}")
+
+#  print(find_sum(l))
 result = find_sum(l)*l[-2]
-print(result)
+print(f"sum = {result}")
