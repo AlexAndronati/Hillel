e622270f0bd6bd402c91db47e1426e5fd0667730
AlexAndronati <55084387+AlexAndronati@users.noreply.github.com>
2020-04-01 17:59:24 +0300

    Update 1.2.py

--- 1.2.py
+++ 1.2.py
@@ -1,14 +1,30 @@
 
 
-def my_range(a, b=0, step=1):
-    if b is my_range.__defaults__[0]:
-        last = a
+def my_range(*args):
+    # if b is my_range.__defaults__[0]:
+    #     last = a
+    #     current = 0
+    # else:
+    #     current = a
+    #     last = b
+    #
+    # step = step
+    #
+    if len(args) == 1:
+        last = args[0]
         current = 0
+        step = 1
+    elif len(args) == 2:
+        current = args[0]
+        last = args[1]
+        step = 1
+    elif len(args) == 3:
+        current = args[0]
+        last = args[1]
+        step = args[2]
     else:
-        current = a
-        last = b
+        raise Exception("Number of arguments should be 1, 2 or 3")
 
-    step = step
     while current < last:
         yield current
         current += step
@@ -27,9 +43,9 @@ if __name__ == '__main__':
         print(i, end=" ")
     print("\n")
 
+    for i in my_range(7, 0):  # Начальное значение больше конечного. Ничего не выводит
+        print(i, end=" ")
+    print("\n")
     
-
-
-
-
-
+    for i in my_range(1, 5, 2, 8):  # ERROR
+        print(i, end=" ")
